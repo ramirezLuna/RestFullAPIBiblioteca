e87daaacc56e98d1d4f71e42d75ab214a964fa24
Your Name <you@example.com>
2023-03-10 09:48:36 -0600

    Agregar archivos de proyecto.

---
+++ RestFullAPIBiblioteca/Aplicacion/Feautres/Libros/Comandos/EliminarLibro/EliminarLibro.cs
@@ -0,0 +1,45 @@
+﻿using Aplicacion.Interfaces;
+using Aplicacion.Wrappers;
+using Dominio.Entidades;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Feautres.Libros.Comandos.EliminarLibro
+{
+    public class EliminarLibro : IRequest<Respuesta<int>>
+    {
+        public int Id { get; set; }
+    }
+    public class EliminarLibroHandler : IRequestHandler<EliminarLibro, Respuesta<int>>
+    {
+        private readonly IRepositorioAsincrono<Libro> _repositorioAsincrono;
+
+        public EliminarLibroHandler(IRepositorioAsincrono<Libro> repositorioAsincrono)
+        {
+            _repositorioAsincrono = repositorioAsincrono;
+        }
+
+       /* public async Task<Unit> Handle(Respuesta<int> request, CancellationToken cancellationToken)
+        {
+           
+            throw new NotImplementedException();
+        }*/
+        public async Task<Respuesta<int>> Handle(EliminarLibro request, CancellationToken cancellationToken)
+        {
+            var Libro = await _repositorioAsincrono.GetByIdAsync(request.Id);
+            if (Libro == null)
+            {
+                throw new KeyNotFoundException($"registro no encontrado en el id{request.Id}");
+            }else
+            {
+                await _repositorioAsincrono.DeleteAsync(Libro);
+                return new Respuesta<int>(Libro.Id);
+            }
+               
+        }
+    }
+}
